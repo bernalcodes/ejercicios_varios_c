2243113c68695defabacfd5147637a8e3843469a
beeeeernal <bernal.felipe98@gmail.com>
2021-06-27 18:04:16 -0500

    changes in pig_ltn.c

--- tercer_nivel/pig_ltn.c
+++ tercer_nivel/pig_ltn.c
@@ -4,11 +4,23 @@
 
 int main(void){
 	char word[60];
+	char ini[1];
+	int i;
 
 	printf("Ingrese la palabra que desea traducida a pig latin: ");
 	gets(word);
 	
-
+	if (word[0] == 'A' || word[0] == 'E' || word[0] == 'I' || word[0] == 'O' || word[0] == 'U'){
+		word[0] = toupper(word[0]);
+		printf("%sway.\n", word);
+	} else {
+		ini[0] = word[0];
+		for (i = 0; i < strlen(word); i++){
+			word[i] = word[i+1];
+		}
+		word[0] = toupper(word[0]);
+		printf("%s%cay.\n", word, ini[0]);
+	}
 
 	return 0;
 }
